55d0c1feb6219c85b4566f18814612e0ae238754
Orlando Eria <lr.oeria@mmdc.mcl.edu.ph>
2026-03-23 19:35:42 +0700

    Update MotorPH_Automatic_Payroll_System.java

--- OneDrive/Documents/NetBeansProjects/MotorPH_Automatic_Payroll_System/src/MotorPH_Automatic_Payroll_System.java
+++ OneDrive/Documents/NetBeansProjects/MotorPH_Automatic_Payroll_System/src/MotorPH_Automatic_Payroll_System.java
@@ -185,8 +185,8 @@ public class MotorPH_Automatic_Payroll_System {
         }
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            // Uncomment next line if attendance file has header:
-            // br.readLine();
+            
+            
 
             String line;
             while ((line = br.readLine()) != null) {
